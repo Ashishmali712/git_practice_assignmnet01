dd1443bda3d5fc80e3427fc5c7f2bd49ebfa7f68
Ashish Mali <maliashish712@gmail.com>
2022-11-03 12:56:14 +0530

    prime_num_code

---
+++ prime.js
@@ -0,0 +1,12 @@
+let count = 0;
+let num = 7;
+for (i = 1; i < num; i++) {
+  if (num % i == 0) {
+    count++;
+  }
+}
+if (count == 0) {
+  console.log(" Not Prime");
+} else {
+  console.log("Prime");
+}
